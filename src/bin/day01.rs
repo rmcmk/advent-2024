b4172d81a92ce5266347c6d971ab747e618bc844
Ryley Kimmel <ryley@hlwgroup.dev>
2024-12-02 10:58:51 -0500

    chore: tidy day 1
    - p2 sorting irrelevant
    - rename `parse_lists` -> `parse_input`

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,12 +1,14 @@
 fn main() {
     p1();
-    p2();
+    p2()
 }
 
 fn p1() {
-    let (first_list, second_list) = parse_lists();
+    let (mut first_list, mut second_list) = parse_input();
+    first_list.sort_unstable();
+    second_list.sort_unstable();
 
-    let total_distance: i64 = first_list
+    let total_distance: i32 = first_list
         .iter()
         .zip(&second_list)
         .map(|(a, b)| (a - b).abs())
@@ -16,35 +18,25 @@ fn p1() {
 }
 
 fn p2() {
-    let (first_list, second_list) = parse_lists();
+    let (first_list, second_list) = parse_input();
 
-    let mut similarity_score: i64 = 0;
+    let mut similarity_score = 0;
     first_list.iter().for_each(|a| {
-        let occurrences = second_list.iter().filter(|b| a == *b).count();
-        similarity_score += a * occurrences as i64;
+        let occurrences = second_list.iter().filter(|b| a == *b).count() as i32;
+        similarity_score += a * occurrences;
     });
 
     println!("P2: Similarity score: {}", similarity_score);
 }
 
-fn parse_lists() -> (Vec<i64>, Vec<i64>) {
-    let input = include_str!("../../input/day01.txt");
-    let mut first_list: Vec<i64> = Vec::new();
-    let mut second_list: Vec<i64> = Vec::new();
-
-    for line in input.lines() {
-        let locations: Vec<_> = line.split_whitespace().collect();
-        let first = locations[0].parse::<i64>().unwrap();
-        let second = locations[1].parse::<i64>().unwrap();
-
-        first_list.push(first);
-        second_list.push(second);
-    }
-
-    assert_eq!(first_list.len(), second_list.len());
-
-    first_list.sort_unstable();
-    second_list.sort_unstable();
-
-    (first_list, second_list)
+fn parse_input() -> (Vec<i32>, Vec<i32>) {
+    include_str!("../../input/day01.txt")
+        .lines()
+        .map(|line| {
+            let mut parts = line.split_whitespace();
+            let first = parts.next().unwrap().parse::<i32>().unwrap();
+            let second = parts.next().unwrap().parse::<i32>().unwrap();
+            (first, second)
+        })
+        .unzip()
 }
